52e247dae6b7e06640d796f394bebfac360cf460
hjhj5291 <39934214+hjhj5291@users.noreply.github.com>
2018-06-08 10:04:55 +0900

    포트폴리오

---
+++ woodamlighting/common/header.php
@@ -0,0 +1,46 @@
+<!-- logo 시작 -->
+                <h1><a href="#">로고</a></h1>
+                <!-- logo 끝 -->
+                
+      
+                
+                <!-- menu 시작 -->
+                <ul class="gnb cf">
+                    <li>
+                        <a href="#">회사소개</a>
+                        
+       
+                    </li>
+                    
+                    <li>
+                        <a href="#">제품소개</a>
+                        
+            
+                    </li>     
+                    
+                    <li>
+                        <a href="#">인증및수상</a>
+               
+                    </li>  
+                    
+                    <li>
+                        <a href="#">포트폴리오</a>
+                        
+  
+                    </li>                    
+                    
+                    <li>
+                        <a href="#">고객센터</a>
+                        
+
+                    </li>                            
+                </ul>
+                <!-- menu  -->
+                
+                <!--topbtn-->
+                <div class="topBtn cf">
+              
+                     <div class="leftBtn"><a href="#"> Product Preview</a></div>
+                     <div class="rightBtn"><a href="#"> Catalogue Download</a></div>
+                </div>
+                <!--topbtn-->
